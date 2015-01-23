21e3fd642dab99ab80281e5fed657337e44514d5
kshitijkesari <kshitij.kesari@vayavyalabs.com>
2015-01-23 18:24:30 +0530

    changes in index.html and mainActivity.java

--- src/com/vayavyalabs/prometheus/MainActivity.java
+++ src/com/vayavyalabs/prometheus/MainActivity.java
@@ -9,11 +9,12 @@ import android.webkit.WebView;
 
 public class MainActivity extends ActionBarActivity {
 
+	
 	@Override
 	protected void onCreate(Bundle savedInstanceState) {
 		super.onCreate(savedInstanceState);
 		setContentView(R.layout.activity_main);
-
+		
 	}
 
 	@Override
@@ -25,9 +26,6 @@ public class MainActivity extends ActionBarActivity {
 
 	@Override
 	public boolean onOptionsItemSelected(MenuItem item) {
-		// Handle action bar item clicks here. The action bar will
-		// automatically handle clicks on the Home/Up button, so long
-		// as you specify a parent activity in AndroidManifest.xml.
 		int id = item.getItemId();
 		if (id == R.id.action_settings) {
 			return true;
